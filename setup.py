6dd4fd362f27955a961451f28c47c27dcfbc584d
eukaryote <sapientdust+github@gmail.com>
2015-02-01 09:57:50 -0800

    use module.__name__ to fix and prevent future typos

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import mkpassphrase
 from setuptools import setup
 
 setup(
-    name='mkpassphrase',
+    name=mkpassphrase.__name__,
     version=mkpassphrase.__version__,
     license='http://www.opensource.org/licenses/mit-license.php',
     description='Word-based passphrase generator',
-    packages=['mkpassphrase'],
+    packages=[mkpassphrase.__name__],
     platforms='any',
     entry_points={
         'console_scripts': [
-            'mkpasshrase = mkpassphrase.main:main',
+            '{name} = {name}.main:main'.format(name=mkpassphrase.__name__),
         ]
     },
 )
