dbdce26ab7f9c1263d4685008763285d62aaef45
eukaryote <sapientdust+github@gmail.com>
2015-02-04 19:01:17 -0800

    pypi fix and version bump

--- mkpassphrase/__init__.py
+++ mkpassphrase/__init__.py
@@ -2,7 +2,7 @@
 Utility methods for generating passphrases from a dictionary file of words.
 """
 
-__version_info = (0, 6, 4)
+__version_info = (0, 6, 5)
 __version__ = '.'.join(map(str, __version_info))
 
 import random
