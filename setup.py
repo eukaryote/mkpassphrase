f739ff0aea476120d3189fdc16dc0adf4a1184ff
eukaryote <sapientdust+github@gmail.com>
2016-07-12 15:20:39 -0700

    fix for install version checking

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools.command.test import test
 
 import mkpassphrase
 
-version = sys.version_info[:2]
+version = sys.version_info[:3]
 
-if version < (2, 7) or (version >= (3, 0) and version < (3, 2, 5)):
-    print('unsupported Python version: %s' % sys.version)
+if version < (2, 7, 0) or (version >= (3, 0, 0) and version < (3, 2, 5)):
+    print('Unsupported Python version: %s' % sys.version_info)
     print('mkpassphrase is supported on python-2.7 and python-3.2.5+')
     sys.exit(1)
 
